174f912c8b5fb9812dca7bef6930f2fd32fee3ac
wen84108 <wen84108@yahoo.com>
2020-03-23 17:45:40 +0000

    Final Project

--- lab06.main.js
+++ lab06.main.js
@@ -182,9 +182,7 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-     let data = this.connector.get(callback);
-
-     console.log("data is " + JSON.stringify(data));
+     this.connector.get(callback);
   }
 
   /**
